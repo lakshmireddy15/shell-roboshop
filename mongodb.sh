786638ecf518c8d33a93b0d31a56b93b62234ea2
lakshmi <nagalakshmireddy67@gmail..com>
2025-05-26 21:19:29 +0530

    shell-roboshop

--- mongodb.sh
+++ mongodb.sh
@@ -13,19 +13,17 @@ LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
-# check the user has root priveleges or not
-if [ $USERID -ne 0 ]
-then
+# Check for root access
+if [ $USERID -ne 0 ]; then
     echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1 #give other than 0 upto 127
+    exit 1
 else
     echo "You are running with root access" | tee -a $LOG_FILE
 fi
 
-# validate functions takes input as exit status, what command they tried to install
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then
+# Validate function
+VALIDATE() {
+    if [ $1 -eq 0 ]; then
         echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
     else
         echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
@@ -33,21 +31,33 @@ VALIDATE(){
     fi
 }
 
-cp $SCRIPT_DIRECTORY/mongo.repo /etc/yum.repos.d/mongodb.repo
-VALIDATE $? "Copying MongoDB repo"
+# Create MongoDB repo file
+cat > /etc/yum.repos.d/mongodb.repo <<EOF
+[mongodb-org-7.0]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/8/mongodb-org/7.0/x86_64/
+enabled=1
+gpgcheck=0
+EOF
 
+VALIDATE $? "Creating MongoDB repo"
+
+# Install MongoDB
 dnf install mongodb-org -y &>>$LOG_FILE
-VALIDATE $? "Installing mongodb server"
+VALIDATE $? "Installing MongoDB server"
 
+# Enable MongoDB
 systemctl enable mongod &>>$LOG_FILE
-VALIDATE $? "Enabling MongoDB"
+VALIDATE $? "Enabling MongoDB service"
 
-
+# Start MongoDB
 systemctl start mongod &>>$LOG_FILE
-VALIDATE $? "Starting MongoDB"
+VALIDATE $? "Starting MongoDB service"
 
+# Update bind IP for remote access
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
-VALIDATE $? "Editing MongoDB conf file for remote connections"
+VALIDATE $? "Updating MongoDB bind IP"
 
+# Restart MongoDB
 systemctl restart mongod &>>$LOG_FILE
-VALIDATE $? "Restarting MongoDB"
+VALIDATE $? "Restarting MongoDB service"
