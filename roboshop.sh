d6d3b41bb33176a64e1253ff08769183dd042093
lakshmi <nagalakshmireddy67@gmail..com>
2025-05-26 19:06:16 +0530

    shell-roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -20,11 +20,11 @@ do
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
-            --query 'Reservations[0].Instances[0].PublicIpAddress' \
+            --query 'Reservations[0].Instances[0].PrivateIpAddress' \
             --output text)
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
-            --query 'Reservations[0].Instances[0].PrivateIpAddress' \
+            --query 'Reservations[0].Instances[0].PublicIpAddress' \
             --output text)
     fi 
 
