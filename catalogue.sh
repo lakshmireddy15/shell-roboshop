7525e671a634bd81c9e9f134113767e28639c1e1
lakshmi <nagalakshmireddy67@gmail..com>
2025-05-27 07:07:24 +0530

    shell-roboshop

--- catalogue.sh
+++ catalogue.sh
@@ -47,7 +47,7 @@ else
     echo "system user si aleday updated"
 fi
 
-mkdir /app 
+mkdir -p /app 
 VALIDATE $? "creating app directory"
 
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>LOG_FILE
